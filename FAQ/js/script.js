c77fa757a65928c6c12b42a2e6edfd2dc76d32cb
Nouman Usman <2022CS49@student.uet.edu.pk>
2024-03-22 19:38:07 +0500

    Devfest added to sponsors

---
+++ FAQ/js/script.js
@@ -0,0 +1,10 @@
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach((toggle) => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
+
+
